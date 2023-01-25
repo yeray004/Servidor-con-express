acc7686e53679978a6f9bd07f13b01a90911bf52
yeray004 <yeye20041@gmail.com>
2023-01-25 12:09:36 -0500

    Despliegue del servidor

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hola mundo")
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hola mundo')
+})
+
+app.listen(port, () => {
+  console.log('Mi puerto es '+ port)
+})
